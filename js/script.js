78bb86de065ee3ee986985821305abe3f8b7ed2e
MarcoBeraldo <beraldomar@gmail.com>
2022-07-05 16:45:00 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+Descrizione:
+// Visualizzare in pagina 5 numeri casuali  diversi tra loro. 
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, 
+// i numeri che ha visto precedentemente, tramite i prompt().
+// Dopo che sono stati inseriti i 5 numeri, 
+// il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+const min = 1;
+const max = 100;
+const totalNumbers = 5;
+
+let time = 30;
